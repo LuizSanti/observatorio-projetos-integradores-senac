88cfb29c0ac1dfa116b1d7ccb019c65e987a5f45
Luiz Gabriel <lgabrielsantiago09@gmail.com>
2026-04-26 21:39:59 -0300

    feat: autenticacao e login com CustomUser

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    PERFIL_CHOICES = [
+        ('aluno', 'Aluno'),
+        ('professor', 'Professor'),
+        ('empresa', 'Empresa'),
+        ('admin', 'Administrador'),
+    ]
+    perfil = models.CharField(
+        max_length=20,
+        choices=PERFIL_CHOICES,
+        default='aluno'
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.get_perfil_display()})"
