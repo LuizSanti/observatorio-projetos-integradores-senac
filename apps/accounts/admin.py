e73a72741b2b5e01ad2d2334c8c82dab072be4c2
Luiz Gabriel <lgabrielsantiago09@gmail.com>
2026-06-04 15:21:24 -0300

    feat: show perfil field in admin use

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-
-from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Perfil", {"fields": ("perfil",)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Perfil", {"fields": ("perfil",)}),
+    )
+
+    list_display = ("username", "email", "perfil", "is_staff")
